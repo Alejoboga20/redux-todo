e42322087b3714b4c7e3f4fbc6bcde890850bbd5
alejoboga <alejoboga20@gmail.com>
2021-03-22 15:57:36 -0300

    Adding MUI

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import Input from './components/Input';
 import './App.css';
+import TodoItem from './components/TodoItem';
+
+const todoList = [
+  { item: 'todo', done: false, id: 123123 },
+  { item: 'todosadas', done: true, id: 12313213 },
+];
 
 function App() {
   return (
     <div className='app'>
-      <Input />
+      <div className='app__container'>
+        <div className='app__todoContainer'>
+          {todoList.map((item) => (
+            <TodoItem name={item.item} done={item.done} id={item.id} />
+          ))}
+        </div>
+        <Input />
+      </div>
     </div>
   );
 }
